21c3b076863062ef86c96d616c6bb0b1eb566022
YOGI <yogibca62@gmail.com>
2021-02-14 23:13:31 +0530

    Add files via upload

---
+++ Day_32/code.py
@@ -0,0 +1,9 @@
+import numpy as np
+from numpy.lib import stride_tricks
+
+def rolling(a, window):
+    shape = (a.size - window + 1, window)
+    strides = (a.itemsize, a.itemsize)
+    return stride_tricks.as_strided(a, shape=shape, strides=strides)
+Z = rolling(np.arange(10), 3)
+print(Z)
